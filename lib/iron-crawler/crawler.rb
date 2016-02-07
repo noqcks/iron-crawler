d394bdc9faedf0961210685626e87d903e26d017
Ben Visser <theodore.r.visser@gmail.com>
2016-02-06 20:18:57 -0500

    the Crawler class is working now!! :beer:

--- lib/iron-crawler/crawler.rb
+++ lib/iron-crawler/crawler.rb
@@ -6,7 +6,8 @@ class Crawler < Mechanize
     @mech = Mechanize.new
   end
 
-  def self.spiderize(url)
+  def spiderize url
+
     @mech.max_history = nil
     stack = @mech.get(url).links
 
@@ -17,11 +18,11 @@ class Crawler < Mechanize
 
       # skip unless host is relative or host is original domain
       # TODO: are we accounting for subdomains?
-      next unless host.nil? or host == agent.history.first.uri.host
+      next unless host.nil? or host == @mech.history.first.uri.host
 
       # skip if we already visited the site
       begin
-        next if agent.visited? site.href
+        next if @mech.visited? site.href
       rescue Mechanize::UnsupportedSchemeError
         puts "skipping #{site.uri}"
         next
@@ -37,40 +38,7 @@ class Crawler < Mechanize
       end
     end
 
-    return agent.history
+    return @mech.history
   end
 end
 
-# # TODO:
-# # - concurrent
-# # - no # hrefs
-# # - link graph using neo4j
-
-# while site = stack.pop
-#   # make sure we really have a uri
-#   next unless site.uri
-#   host = site.uri.host
-
-#   # skip unless host is relative or host is original domain
-#   # TODO: are we accounting for subdomains?
-#   next unless host.nil? or host == agent.history.first.uri.host
-
-#   # skip if we already visited the site
-#   begin
-#     next if agent.visited? site.href
-#   rescue Mechanize::UnsupportedSchemeError
-#     puts "skipping #{site.uri}"
-#     next
-#   end
-
-#   # crawl the URL
-#   puts "crawling #{site.uri}"
-#   begin
-#     page = site.click
-#     next unless Mechanize::Page === page
-#     stack.push(*page.links)
-#   rescue Mechanize::ResponseCodeError
-#   end
-# end
-
-# p agent.history
